bde1376193bc6a3a18686496de129d43e4d1b822
Admin <admin@Admins-MacBook-Pro.local>
2020-03-16 20:41:33 -0700

    Store working. Was missing Store Provider function for useContext

--- rmchooser/src/App.tsx
+++ rmchooser/src/App.tsx
@@ -3,53 +3,57 @@ import './App.css';
 import { Store } from './Store';
 // import axios from 'axios';
 
-function App() {
-	const { state, dispatch } = useContext(Store);
+export default function App(): JSX.Element {
+  const { state, dispatch } = React.useContext(Store);
 
-	useEffect(() => {
-		state.episodes.length === 0 && fetchDataAction();
-	});
+  console.log(
+    'FROM APP.TSX: just destructured state and dispatch',
+    state,
+    'and ',
+    dispatch
+  );
+  React.useEffect(() => {
+    !state && fetchDataAction();
+  });
 
-	const fetchDataAction = async () => {
-		const URL =
-			'https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes';
-		const data = await fetch(URL);
-		const dataJSON = await data.json();
-		console.log('this is data coming through', dataJSON);
-		return dispatch({
-			type: 'FETCH_DATA',
-			payload: dataJSON._embedded.episodes
-		});
-	};
-
-	return (
-		<div className='App'>
-			{console.log('FROM APP.TSX: this is state', state)}
-			<header className='App-header'></header>
-			<h1>Pick your favorite episode!</h1>
-			<h1>{}</h1>
-			<div>
-				<div>
-					<h1>
-						{/* {state.episodes ? (
-							<div>
-								<h1>{state.episodes}</h1>
-							</div>
-						) : (
-							<div>
-								<h3>episodes DOES NOT exist yet</h3>
-							</div>
-						)} */}
-					</h1>
-				</div>
-				<span>
-					<button>+</button>
-					<button>-</button>
-					<button>0</button>
-				</span>
-			</div>
-		</div>
-	);
+  const fetchDataAction = async () => {
+    const URL =
+      'https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes';
+    const data = await fetch(URL);
+    const dataJSON = await data.json();
+    // const dataJSON = await data.json();
+    // console.log(dataJSON._embedded.episodes);
+    console.log('data returned: ', dataJSON);
+    return dispatch({
+      type: 'FETCH_DATA',
+      payload: dataJSON._embedded.episodes
+    });
+  };
+  return (
+    <div className='App'>
+      {console.log('FROM APP.TSX: this is state', state)}
+      <header className='App-header'></header>
+      <h1>Pick your favorite episode!</h1>
+      <div>
+        <div>
+          <h1>
+            {/* {state.episodes ? (
+              <div>
+                <h1>{state.episodes}</h1>
+              </div>
+            ) : (
+              <div>
+                <h3>episodes DOES NOT exist yet</h3>
+              </div>
+            )} */}
+          </h1>
+        </div>
+        <span>
+          <button>+</button>
+          <button>-</button>
+          <button>0</button>
+        </span>
+      </div>
+    </div>
+  );
 }
-
-export default App;
--- rmchooser/src/Store.tsx
+++ rmchooser/src/Store.tsx
@@ -2,37 +2,40 @@ import React, { useReducer } from 'react';
 import { ReactComponent } from '*.svg';
 
 interface IState {
-	episodes: Array<number>;
-	favorites: Array<string>;
+  episodes: [];
+  favorites: [];
 }
 
 interface IAction {
-	type: string;
-	payload: any;
+  type: string;
+  payload: any;
 }
 
 const initialState: IState = {
-	episodes: [1, 2, 3],
-	favorites: []
+  episodes: [],
+  favorites: []
 };
 
 export const Store = React.createContext<IState | any>(initialState);
 
 // your reducer function
 // can modularize if gets big
-export const reducer = (state: IState, action: IAction): IState => {
-	switch (action.type) {
-		// when returning inside a reducer function, return the entire state using the spread operator and then the next argument will be the specific properties of the state that you want to update
-		case 'FETCH_DATE':
-			return { ...state, episodes: action.payload };
-		default:
-			return state;
-	}
-};
+function reducer(state: IState, action: IAction): IState {
+  switch (action.type) {
+    // when returning inside a reducer function, return the entire state using the spread operator and then the next argument will be the specific properties of the state that you want to update
+    case 'FETCH_DATA':
+      return { ...state, episodes: action.payload };
+    default:
+      return state;
+  }
+}
 
 export const StoreProvider = (props: any): JSX.Element => {
-	const [state, dispatch] = useReducer(reducer, initialState);
-	return (
-		<Store.Provider value={{ state, dispatch }}>{props.children}</Store.Provider>
-	);
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+  console.log('STORE.TSX state from provider', state);
+  return (
+    <Store.Provider value={{ state, dispatch }}>
+      {props.children}
+    </Store.Provider>
+  );
 };
